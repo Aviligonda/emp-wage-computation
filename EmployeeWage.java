3298230d070a1e19060093cfe7e56f69166ed5e6
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-06-08 10:39:16 +0530

    modified: Solving using switch case method

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,22 +1,25 @@
 public class EmployeeWage {
     public static final int wagePerHr = 20;
-    public static final int fullDayHr = 8;
-    public static final int halfDayHr = 4;
 
     public static void main(String[] args) {
-        int dailyWage;
+        int dailyWage, empHr = 0;
         System.out.println("Welcome to Employee Wage Computation Program");
         int check = (int) (Math.random() * 3);
-        if (check == 1) {
-            System.out.println("Employee is present in full day");
-            dailyWage = wagePerHr * fullDayHr;
-            System.out.println("Daily wage is :" + dailyWage);
-        } else if (check == 2) {
-            System.out.println("Employee is present in half day");
-            dailyWage = wagePerHr * halfDayHr;
-            System.out.println("Daily wage is :" + dailyWage);
-        } else {
-            System.out.println("Employee is absent");
+        switch (check) {
+            case 1:
+                System.out.println("Employee present in full day");
+                empHr = 8;
+                break;
+            case 2:
+                System.out.println("Employee present in half day");
+                empHr = 4;
+                break;
+            default:
+                System.out.println("Employee is absent");
+                empHr = 0;
+                break;
         }
+        dailyWage = empHr * wagePerHr;
+        System.out.println("Daily wage is :" + dailyWage);
     }
 }
