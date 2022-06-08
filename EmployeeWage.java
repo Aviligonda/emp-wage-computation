b89b24d955462a5fbf00ba2b5f7e8aad34ffba1d
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-06-08 10:18:07 +0530

    Added: Employee present or not

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,5 +1,11 @@
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        int check = (int) (Math.random() * 2);
+        if (check == 1) {
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee is absent");
+        }
     }
 }
