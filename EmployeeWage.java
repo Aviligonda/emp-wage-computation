ad423bf8bf9be77b6518db5498c735390d3f8496
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-06-08 11:35:42 +0530

    Modified : Cal total emp wage for multiple companys

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,41 +1,37 @@
 public class EmployeeWage {
-    public static final int wagePerHr = 20;
-    public static final int totalWorkingDays = 20;
-    public static final int totalWorkingHr = 100;
 
     public static void main(String[] args) {
         EmployeeWage employeeWage = new EmployeeWage();
-        employeeWage.calWage();
+        employeeWage.calWageComputation("TCS", 20, 20, 100);
+        System.out.println("---------------------------------------------------------------------------");
+        employeeWage.calWageComputation("Info", 25, 25, 90);
+        System.out.println("---------------------------------------------------------------------------");
+        employeeWage.calWageComputation("TATA", 30, 31, 110);
+
     }
 
-    public void calWage() {
-        int dailyWage, empHr = 0, monthlyWage = 0, maxWorkingDays = 0, maxWorkingHr = 0;
+    public void calWageComputation(String company, int wagePerHr, int totalWorkingDays, int totalWorkingHr) {
+        int empHr, maxWorkingDays = 0, maxWorkingHr = 0;
         System.out.println("Welcome to Employee Wage Computation Program");
         while (maxWorkingHr <= totalWorkingHr && maxWorkingDays < totalWorkingDays) {
             maxWorkingDays++;
             int check = (int) (Math.random() * 3);
             switch (check) {
                 case 1:
-                    System.out.println("Employee present in full day");
                     empHr = 8;
                     break;
                 case 2:
-                    System.out.println("Employee present in half day");
                     empHr = 4;
                     break;
                 default:
-                    System.out.println("Employee is absent");
                     empHr = 0;
                     break;
             }
 
             maxWorkingHr += empHr;
-            System.out.println("Day#" + maxWorkingDays + "Employee hours :" + empHr);
 
         }
         int totalEmpWage = maxWorkingHr * wagePerHr;
-        System.out.println("Total employee wage is :" + totalEmpWage);
-
+        System.out.println(company + " --> Company Total Employee wage is :" + totalEmpWage);
     }
-
 }
