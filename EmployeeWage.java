42516cb08330fff7619974376255d6d6cf5b03dc
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-06-08 11:00:00 +0530

    modified :Calculating wage till condition reached

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,28 +1,34 @@
 public class EmployeeWage {
     public static final int wagePerHr = 20;
     public static final int totalWorkingDays = 20;
+    public static final int totalWorkingHr = 100;
 
     public static void main(String[] args) {
-        int dailyWage, empHr = 0, monthlyWage;
+        int dailyWage, empHr = 0, monthlyWage = 0, maxWorkingDays = 0, maxWorkingHr = 0;
         System.out.println("Welcome to Employee Wage Computation Program");
-        int check = (int) (Math.random() * 3);
-        switch (check) {
-            case 1:
-                System.out.println("Employee present in full day");
-                empHr = 8;
-                break;
-            case 2:
-                System.out.println("Employee present in half day");
-                empHr = 4;
-                break;
-            default:
-                System.out.println("Employee is absent");
-                empHr = 0;
-                break;
+        while (maxWorkingHr <= totalWorkingHr && maxWorkingDays < totalWorkingDays) {
+            maxWorkingDays++;
+            int check = (int) (Math.random() * 3);
+            switch (check) {
+                case 1:
+                    System.out.println("Employee present in full day");
+                    empHr = 8;
+                    break;
+                case 2:
+                    System.out.println("Employee present in half day");
+                    empHr = 4;
+                    break;
+                default:
+                    System.out.println("Employee is absent");
+                    empHr = 0;
+                    break;
+            }
+
+            maxWorkingHr += empHr;
+            System.out.println("Day#" + maxWorkingDays + "Employee hours :" + empHr);
+
         }
-        dailyWage = empHr * wagePerHr;
-        System.out.println("Daily wage is :" + dailyWage);
-        monthlyWage = dailyWage * totalWorkingDays;
-        System.out.println("Monthly wage is :" + monthlyWage);
+        int totalEmpWage = maxWorkingHr * wagePerHr;
+        System.out.println("Total employee wage is :" + totalEmpWage);
     }
 }
